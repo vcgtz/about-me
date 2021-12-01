a3a1478b09aa685336e80e491e73efdb11e06881
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-12-01 13:46:35 -0600

    Change the principal message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello! This is an example ot use a VueJS app in Github Pages"/>
 </template>
 
 <script>
