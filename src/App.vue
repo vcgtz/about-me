899bf47f2aab176a7d20cb85269b539bae5171fd
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-12-09 22:52:29 -0600

    ✨ Create base components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <template>
-  <p class="text-blue-500">Hello World!</p>
+<div>
+  <main class="dark:bg-gray-800 font-mono bg-white relative overflow-hidden h-screen">
+    <Header />
+    
+    <div class="flex relative z-20 items-center">
+      <div class="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
+        <div class="flex flex-col">
+          <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" class="rounded-full w-28 mx-auto"/>
+          <p class="text-3xl my-6 text-center dark:text-white">Hi, I&#x27;m Vicente 🤘</p>
+          
+          <h2 class="max-w-3xl text-5xl md:text-6xl font-bold mx-auto dark:text-white text-gray-800 text-center py-2">
+            Software Engineer & Gamer
+          </h2>
+          
+          <div class="flex items-center justify-center mt-4">
+            <a href="#" class="uppercase py-2 my-2 px-4 md:mt-16 bg-transparent dark:text-gray-800 dark:bg-white hover:dark:bg-gray-100 border-2 border-gray-800 text-gray-800 dark:text-white hover:bg-gray-800 hover:text-white text-md line-through">
+              CONNECT WITH ME
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+
 export default {
-  name: 'App'
+  components: { Header },
+  name: 'App',
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="h-24 sm:h-32 flex items-center z-30 w-full">
+    <div class="container mx-auto px-6 flex items-center justify-between">
+      <MainLogo />
+      
+      <div class="flex items-center">
+        <nav class="font-sen text-gray-800 dark:text-white uppercase text-lg lg:flex items-center hidden">
+          <a href="#" class="py-2 px-6 flex hover:text-black line-through">Resume</a>
+          <a href="#" class="py-2 px-6 flex hover:text-black line-through">Blog</a>
+          <a href="#" class="py-2 px-6 flex hover:text-black line-through">Contact</a>
+        </nav>
+        
+        <button class="lg:hidden flex flex-col ml-4">
+          <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1"></span>
+          <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1"></span>
+          <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import MainLogo from './MainLogo.vue';
+
+export default {
+  components: { MainLogo },
+  name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
