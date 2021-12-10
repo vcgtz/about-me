24668070e597f59916120941b8b7da0a44bcfd71
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-12-09 21:55:17 -0600

    💅  Add and configure tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hello! This is an example ot use a VueJS app in Github Pages"/>
+  <p class="text-blue-500">Hello World!</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
